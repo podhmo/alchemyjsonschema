157bd9949a06d1b772c08f34bdc29972a09786d1
podhmo <podhmo+altair@beproud.jp>
2014-05-21 22:26:15 +0900

    pep8

--- alchemyjsonschema/tests/test_it.py
+++ alchemyjsonschema/tests/test_it.py
@@ -3,6 +3,7 @@ def _getTarget():
     from alchemyjsonschema import SchemaFactory
     return SchemaFactory
 
+
 def _makeOne(*args, **kwargs):
     from alchemyjsonschema import (
         SingleModelWalker, 
@@ -11,11 +12,12 @@ def _makeOne(*args, **kwargs):
     return _getTarget()(SingleModelWalker, DefaultClassfier)
 
 
-## definition
+# definition
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class Group(Base):
     """model for test"""
@@ -25,12 +27,14 @@ class Group(Base):
     name = sa.Column(sa.String(255), default="", nullable=False)
     color = sa.Column(sa.Enum("red", "green", "yellow", "blue"))
 
+
 def test_type__is_object():
     target = _makeOne()
     result = target.create(Group)
 
     assert "type" in result
     assert result["type"] == "object"
+
 
 def test_properties__are__all_of_columns():
     target = _makeOne()
@@ -39,12 +43,14 @@ def test_properties__are__all_of_columns():
     assert "properties" in result
     assert list(sorted(result["properties"].keys())) == ["color", "name", "pk"]
 
+
 def test_required__are__nullable_is_false_columns():
     target = _makeOne()
     result = target.create(Group)
 
     assert "required" in result
     assert list(sorted(result["required"])) == ["name", "pk"]
+
 
 def test_title__id__model_class_name():
     target = _makeOne()
@@ -53,6 +59,7 @@ def test_title__id__model_class_name():
     assert "title" in result
     assert result["title"] == Group.__name__
 
+
 def test_description__is__docstring_of_model():
     target = _makeOne()
     result = target.create(Group)
@@ -60,7 +67,8 @@ def test_description__is__docstring_of_model():
     assert "description" in result
     assert result["description"] == Group.__doc__
 
-def test_properties__all__this_is_slackoff_little_bit__all_is_all(): #hmm.
+
+def test_properties__all__this_is_slackoff_little_bit__all_is_all():   # hmm.
     target = _makeOne()
     result = target.create(Group)
 
@@ -70,18 +78,20 @@ def test_properties__all__this_is_slackoff_little_bit__all_is_all(): #hmm.
                                     'name': {'maxLength': 255, 'type': 'string'},
                                     'pk': {'description': 'primary key', 'type': 'integer'}}
 
+# adaptive
 
 
-### adaptive
 def test__filtering_by__includes():
     target = _makeOne()
     result = target.create(Group, includes=["pk"])
     assert list(sorted(result["properties"].keys())) == ["pk"]
 
+
 def test__filtering_by__excludes():
     target = _makeOne()
     result = target.create(Group, excludes=["pk"])
     assert list(sorted(result["properties"].keys())) == ["color", "name"]
+
 
 def test__filtering_by__excludes_and_includes__conflict():
     import pytest
@@ -91,14 +101,15 @@ def test__filtering_by__excludes_and_includes__conflict():
     with pytest.raises(InvalidStatus):
         target.create(Group, excludes=["pk"], includes=["pk"])
 
+# overrides
 
 
-### overrides
 def test__overrides__add():
     target = _makeOne()
     overrides = {"name": {"maxLength": 100}}
     result = target.create(Group, includes=["name"], overrides=overrides)
     result["properties"] == {"name": {"maxLength": 100, 'type': 'string'}}
+
 
 def test__overrides__pop():
     from alchemyjsonschema import pop_marker
@@ -106,6 +117,7 @@ def test__overrides__pop():
     overrides = {"name": {"maxLength": pop_marker}}
     result = target.create(Group, includes=["name"], overrides=overrides)
     result["properties"] == {"name": {'type': 'string'}}
+
 
 def test__overrides__wrong_column():
     import pytest
