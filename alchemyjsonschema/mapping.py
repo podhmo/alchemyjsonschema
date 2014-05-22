79b854d2844d20d0bd32f18b9acf7a4098ee4151
podhmo <podhmo+altair@beproud.jp>
2014-05-23 06:02:25 +0900

    add mapping class. this is convinience wrapper

---
+++ alchemyjsonschema/mapping.py
@@ -0,0 +1,75 @@
+# -*- coding:utf-8 -*-
+from functools import partial
+from .dictify import (
+    objectify,
+    jsonify,
+    dictify,
+    ModelLookup
+)
+from jsonschema import (
+    validate,
+    FormatChecker
+)
+from jsonschema.validators import (
+    Draft3Validator,
+    Draft4Validator
+)
+
+
+class Mapping(object):
+    def __init__(self, validator, model, modellookup):
+        self.validator = validator
+        self.format_checker = validator.format_checker
+        self.schema = validator.schema
+        self.model = model
+        self.modellookup = modellookup
+
+    def jsondict_from_object(self, ob):
+        return jsonify(ob, self.schema)
+
+    def dict_from_object(self, ob):
+        return dictify(ob, self.schema)
+
+    def object_from_dict(self, params, strict=True):
+        return objectify(params, self.schema, self.modellookup, strict=strict)
+
+    def validate_jsondict(self, jsondict):
+        return validate(jsondict, self.schema, format_checker=self.format_checker)
+
+    def validate_all_jsondict(self, jsondict):
+        return validate_all(jsondict, self.validator)
+
+
+class ErrorFound(Exception):  # xxx:
+    pass
+
+
+def validate_all(data, validator):
+    errors = []
+    for e in validator.iter_errors(data):
+        errors.append(dict(name=e.path[0], reason=e.validator))
+    if errors:
+        raise ErrorFound(errors)
+    return None
+
+
+class MappingFactory(object):
+    _Mapping = Mapping
+    _ModelLookup = ModelLookup
+
+    def __init__(self, validator_class, schema_factory, module, resolver=None, format_checker=None):
+        self.schema_factory = schema_factory
+        self.validator_class = validator_class
+        self.resolver = resolver
+        self.format_checker = format_checker or FormatChecker()
+        self.module = module
+
+    def create(self, model, includes=None, excludes=None, depth=None):
+        schema = self.schema_factory.create(model, includes=includes, excludes=excludes, depth=depth)
+        validator = self.validator_class(schema, resolver=self.resolver, format_checker=self.format_checker)
+        modellookup = self._ModelLookup(self.module)
+        mapping = self._Mapping(validator, model, modellookup)
+        return mapping
+
+Draft3MappingFactory = partial(MappingFactory, Draft3Validator)
+Draft4MappingFactory = partial(MappingFactory, Draft4Validator)
---
+++ alchemyjsonschema/tests/test_it_mapping.py
@@ -0,0 +1,104 @@
+# -*- coding:utf-8 -*-
+"""
+jsondict <-> dict <-> model object
+   \______________________/
+"""
+
+
+def _getTarget():
+    from alchemyjsonschema.mapping import Draft4MappingFactory
+    return Draft4MappingFactory
+
+
+def _makeOne(schema_factory, model, *args, **kwargs):
+    import alchemyjsonschema.tests.models as models
+    module = models
+    mapping_factory = _getTarget()(schema_factory, module, *args, **kwargs)
+    return mapping_factory.create(model)
+
+
+def test_it__dict_from_model_object():
+    from alchemyjsonschema import AlsoChildrenWalker, SchemaFactory
+    from .models import Group, User
+    from datetime import datetime
+
+    schema_factory = SchemaFactory(AlsoChildrenWalker)
+    target = _makeOne(schema_factory, Group)
+
+    group = Group(name="ravenclaw", color="blue", created_at=datetime(2000, 1, 1, 10))
+    group.users = [User(name="foo", created_at=datetime(2000, 1, 1, 10))]
+
+    dict_ = target.dict_from_object(group)
+    assert dict_ == {'color': 'blue',
+                     'users': [{'created_at': datetime(2000, 1, 1, 10, 0), 'pk': None, 'name': 'foo'}],
+                     'created_at': datetime(2000, 1, 1, 10, 0),
+                     'pk': None,
+                     'name': 'ravenclaw'}
+
+
+def test_it__jsondict_from_model():
+    from alchemyjsonschema import AlsoChildrenWalker, SchemaFactory
+    from .models import Group, User
+    from datetime import datetime
+
+    schema_factory = SchemaFactory(AlsoChildrenWalker)
+    target = _makeOne(schema_factory, Group)
+
+    group = Group(name="ravenclaw", color="blue", created_at=datetime(2000, 1, 1, 10))
+    group.users = [User(name="foo", created_at=datetime(2000, 1, 1, 10))]
+
+    jsondict = target.jsondict_from_object(group)
+
+    import json
+    assert json.dumps(jsondict)
+
+    assert jsondict == {'color': 'blue',
+                        'name': 'ravenclaw',
+                        'users': [{'name': 'foo', 'pk': None, 'created_at': '2000-01-01T10:00:00Z'}],
+                        'pk': None,
+                        'created_at': '2000-01-01T10:00:00Z'}
+
+
+def test_it__validate__jsondict():
+    from alchemyjsonschema import AlsoChildrenWalker, SchemaFactory
+    from .models import Group
+
+    schema_factory = SchemaFactory(AlsoChildrenWalker)
+    target = _makeOne(schema_factory, Group)
+
+    group_dict = {'color': 'blue',
+                  'name': 'ravenclaw',
+                  'users': [{'name': 'foo', 'pk': 10, 'created_at': '2000-01-01T10:00:00Z'}],
+                  'pk': 1,
+                  'created_at': '2000-01-01T10:00:00Z'}
+
+    target.validate_jsondict(group_dict)
+
+
+def test_it__object_from_dict():
+    from alchemyjsonschema import AlsoChildrenWalker, SchemaFactory
+    from .models import Group, User
+    from datetime import datetime
+
+    schema_factory = SchemaFactory(AlsoChildrenWalker)
+    target = _makeOne(schema_factory, Group)
+
+    group_dict = {'color': 'blue',
+                  'users': [{'created_at': datetime(2000, 1, 1, 10, 0), 'pk': None, 'name': 'foo'}],
+                  'created_at': datetime(2000, 1, 1, 10, 0),
+                  'pk': None,
+                  'name': 'ravenclaw'}
+
+    group = target.object_from_dict(group_dict, strict=False)
+
+    assert isinstance(group, Group)
+    assert group.color == "blue"
+    assert group.name == "ravenclaw"
+    assert group.pk is None
+    assert group.created_at == datetime(2000, 1, 1, 10)
+
+    assert (len(group.users) == 1) and (isinstance(group.users[0], User))
+    assert group.users[0].name == "foo"
+    assert group.users[0].pk is None
+    assert group.users[0].created_at == datetime(2000, 1, 1, 10)
+
