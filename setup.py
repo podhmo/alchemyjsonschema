f34752678960b247cfac87c4203c49e712a453d6
podhmo <podhmo+altair@beproud.jp>
2016-03-04 20:40:37 +0900

    bump version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class PyTest(TestCommand):
 
 
 setup(name='alchemyjsonschema',
-      version='0.2.3',
+      version='0.3.0',
       description='mapping jsonschema for sqlalchemy models',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
