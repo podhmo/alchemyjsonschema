18d096e7ef9194283efb8686ba8a80e93fb8263e
podhmo <podhmo+altair@beproud.jp>
2016-03-04 20:53:12 +0900

    forgetting to remove *.egg-info file

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class PyTest(TestCommand):
 
 
 setup(name='alchemyjsonschema',
-      version='0.3.1',
+      version='0.3.1.1',
       description='mapping jsonschema for sqlalchemy models',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
