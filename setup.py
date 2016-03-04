b7b9d3100d97daeb6bca04efc8b5daf0720ee8ac
podhmo <podhmo+altair@beproud.jp>
2016-03-04 20:40:12 +0900

    lint

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
-
 import os
-import sys
+
 
 from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
@@ -13,32 +12,35 @@ try:
 except IOError:
     README = CHANGES = ''
 
-install_requires=[
+install_requires = [
     'setuptools',
     'sqlalchemy',
     'jsonschema',
     'strict-rfc3339',
     'isodate',  # hmm.
     'pytz'
-    ]
+]
 
 docs_extras = [
-    ]
+]
 
-tests_require =[
-    "pytest", 
+tests_require = [
+    "pytest",
     "webob"
 ]
+
 testing_extras = tests_require + [
-    ]
+]
 
 from setuptools.command.test import test as TestCommand
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         import pytest
         pytest.main(self.test_args)
@@ -47,11 +49,11 @@ class PyTest(TestCommand):
 setup(name='alchemyjsonschema',
       version='0.2.3',
       description='mapping jsonschema for sqlalchemy models',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: Implementation :: CPython",
-        ],
+          "Programming Language :: Python",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
       keywords='alchemyjsonschema sqlalchemy jsonschema schema-generation',
       author="podhmo",
       author_email="",
@@ -59,19 +61,16 @@ setup(name='alchemyjsonschema',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires = install_requires,
-      extras_require = {
-          'testing':testing_extras,
-          'docs':docs_extras,
-          },
-      tests_require = tests_require,
-      cmdclass = {'test': PyTest},
+      install_requires=install_requires,
+      extras_require={
+          'testing': testing_extras,
+          'docs': docs_extras,
+      },
+      tests_require=tests_require,
+      cmdclass={'test': PyTest},
       license="mit",
-      entry_points = """
+      entry_points="""
       [console_scripts]
       alchemyjsonschema = alchemyjsonschema.command:main
       """
-      )
-
-
-
+)
