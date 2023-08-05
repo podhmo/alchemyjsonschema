7f6c463836645d9dfb907c536cef53964a20c04c
podhmo <ababjam61+github@gmail.com>
2023-08-05 20:32:28 +0900

    chore: bump version

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 try:
     with open(os.path.join(here, "README.rst")) as f:
         README = f.read()
-    with open(os.path.join(here, "CHANGES.txt")) as f:
-        CHANGES = f.read()
 except IOError:
     README = CHANGES = ""
 
